7a03f7a4355af18f9fda20171e4e057678b37d0d
nemethkoppany <koppany344@gmail.com>
2025-04-05 17:01:38 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 /**
  * 
- * @param {string} className 
+ * @param {string} NameOfTheClass 
  * @returns {HTMLDivElement}
  */
-const makeDiv = (className) => {//Arrow function egy classname bemeneti paraméterrel
+const divMaker = (NameOfTheClass) => {//Arrow function egy bemeneti paraméterrel
     const div = document.createElement("div");//div element létrehozása
-    div.className = className;//Ennek a div elementnek adunk egy className-et (Ezt a classname-et majd mi adjuk meg)
+    div.className = NameOfTheClass;//Ennek a div elementnek adunk egy className-et (Ezt a classname-et majd mi adjuk meg)
     return div;//Visszatérünk a divvel
 }
 
-const containerDiv = makeDiv("container");//Készítünk egy div-et aminke a class-a container lesz
+const containerDiv = divMaker("container");//Készítünk egy div-et aminke a class-a container lesz
 document.body.appendChild(containerDiv);//Hozzárakjuk a body-hoz a containerDiv-et
-const tableDiv = document.createElement("table");//Készítünk egy table elementet
+const tableDiv = divMaker("table");//Készítünk egy table div-et
 
-const formDiv = makeDiv("form");//Készítünk egy divet melynek a class-a form lesz
+const tableElement = document.createElement("table");//Készítünk egy HTMLelemet és azt eltároljuk egy változóban
+tableDiv.appendChild(tableElement);//Hozzáadjuk a tableDiv-hez
+
+const header = document.createElement("thead");//Készítünk egy HTMLelemet és azt eltároljuk egy változóban
+tableElement.appendChild(header);//Hozzáadjuk a tableElement-hez
+
+const headerRow = document.createElement("tr");//Készítünk egy HTMLelemet és azt eltároljuk egy változóban
+header.appendChild(headerRow);//Hozzáadjuk a header-höz
+
+const headerText = ["forradalom", "évszám", "sikeres"];//Tömb amiben a fejléc tartalma van
+
+for(const headerCellText of headerText){//Végigmegyünk ezen a tömbön
+    const headerCell = document.createElement("th");//Készítünk egy HTMLelemet és azt eltároljuk egy változóban
+    headerCell.innerText = headerCellText;//Az aktuális elem belekerül a cellába
+    headerRow.appendChild(headerCell);//Hozzáadjuk a headerCell-t a headerRow-hoz
+}
+
+const formDiv = divMaker("form");//Készítünk egy divet melynek a class-a form lesz
 
 containerDiv.appendChild(tableDiv);//Hozzárakjuk a containerdiv-hez a tableDiv-et
 containerDiv.appendChild(formDiv);//Hozzárakjuk a containerDiv-hez a formDiv-et
