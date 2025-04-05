d6ece2d42c75f6faf8174112340a91299dc8de03
nemethkoppany <koppany344@gmail.com>
2025-04-05 22:22:48 +0200

    form letrehozasa sima

--- main.js
+++ main.js
@@ -32,5 +32,59 @@ for(const headerCellText of headerText){//Végigmegyünk ezen a tömbön
 
 const formDiv = divMaker("form");//Készítünk egy divet melynek a class-a form lesz
 
+const Simaform = document.createElement("form");//Készytünk egy formot
+formDiv.appendChild(Simaform);//Ezt hozzérakjuk a formDiv-hez
+const fieldElements = [{//Egx tümb deklarálása, amiben 3 objektum lesz
+    fieldid: "revolution",//Az első input id-ja
+    fieldLabel: "forrdalom"//Az első input fölé ez lesz írva
+},
+{
+    fieldid: "year",//A második input id-ja
+    fieldLabel: "évszám"//Az második  input fölé ez lesz írva
+},
+{
+    fieldid: "success",//A legördülő menü id-je
+    fieldLabel: "sikeres"//A legördülő menü fölé ez lesz írva
+}
+];
+
+for(const fieldElement of fieldElements){//Végigmegyünk a tömbön
+    const field = divMaker("field");//készítünk egy divet field class névvel
+    Simaform.appendChild(field);//Hozzárakjuk a form-hoz a field-et
+
+    const label = document.createElement("label");//Készítünk egy labelt
+    label.htmlFor = fieldElement.fieldid;//A label "id"-je (vagy inkább for-ja) az aktuális elem id-je lesz
+    label.innerHTML = fieldElement.fieldLabel;//Ugyan így a textContent
+    field.appendChild(label);//És ezt hozzárakjuk a fieldhez
+
+
+    if(fieldElement.fieldid === "success"){//Hogyha a fejlécnél elértünk a "sikeres" mezőhöz
+        input = document.createElement("select");//Hozzunk létre egy legördülő menüt
+        input.id = fieldElement.fieldid;//Adjuk meg a select id-jét
+
+        const option1 = document.createElement("option");//Első opció
+        option1.value = "yes";//Az értéke yes
+        option1.innerHTML = "igen";//Anmit kiír: igen
+
+        const option2 = document.createElement("option");//Első opció
+        option2.value = "no";//Az értéke no
+        option2.innerHTML = "nem";//Anmit kiír: nem
+
+        input.appendChild(option1);//Hozzárakjuk az opciót a fieldhez
+        input.appendChild(option2);//Hozzárakjuk az opciót a fieldhez
+
+        field.appendChild(document.createElement("br"));//Csinálunk egy brake-et is, hogy egymás alattt legyen a szöveg és a mező
+    }
+    else{//Ha még nem vagyunk a selectnél
+        input = document.createElement("input");//Csak input fieldet hozzunk létre
+        input.id = fieldElement.fieldid;//Aminek ugyan az az id-je mint ami az objektumok tuéajdonságainak
+        field.appendChild(document.createElement("br"));//Csinálunk egy brake-et is, hogy egymás alattt legyen a szöveg és legördülő menü
+    }
+    field.appendChild(input);//Ezt az egészet belerakjuk a field-be
+}
+const button = document.createElement("button");//Készítünk egy gombot
+button.textContent = "Hozzáadás";//Amibe ez lesz írva
+Simaform.appendChild(button);//És azt hozzárakjuk a fprm-hoz
+
 containerDiv.appendChild(tableDiv);//Hozzárakjuk a containerdiv-hez a tableDiv-et
 containerDiv.appendChild(formDiv);//Hozzárakjuk a containerDiv-hez a formDiv-et
